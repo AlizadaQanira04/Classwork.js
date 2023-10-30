6af0b56a01065ac0f2bad308bee4115854bc0e07
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-10-30 08:52:17 -0700

    js

---
+++ 01.Data-Types&Variables&Operators/app.js
@@ -0,0 +1,46 @@
+//TASK1
+let firtsName = "Qanira";
+let lastName = "Alizade";
+let fullName = firtsName + " " + lastName;
+let country = " Azerbaijan";
+let age = 19 ;
+let job = "student";
+let personalInfo;
+personalInfo =
+  fullName + " l live in" + country + " l am " + age + "l am a" + job;
+console.log(personalInfo);
+
+let length = 16; // number 
+let color = "Brown"; //string
+let float = "12.5"; //string
+let like = true; //sstring
+let x ={
+    firtsName:"Nick",
+    color:"red",
+}; //object
+console.log(typeof length);
+console.log(typeof color);
+console.log(typeof float);
+console.log(typeof like);
+console.log(typeof x);
+
+//TASK 2
+
+let distance = 8600;
+let speed = 120;
+let duration = (distance/speed)*60;
+console .log(" The car will arrive in China in " + duration + " minutes ");
+
+//TASK2
+let birthDate = 2004;
+let date = 2023;
+let year = date - birthDate;
+console .log(" my years is " + year);
+
+//TASK3
+let nargiz = 29;
+let sabina = 30;
+let aziza = 40;
+let arkinaz = 50 ;
+let sum = ( nargiz + sabina + aziza + arkinaz) /4;
+console .log(sum);
