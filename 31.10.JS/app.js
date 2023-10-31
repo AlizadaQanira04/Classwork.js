e1d870ac7cf127499d6075f47d59ccd74966630c
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-10-31 08:52:54 -0700

    classwork-js2

---
+++ 31.10.JS/app.js
@@ -0,0 +1,80 @@
+//Task 1
+// let weather = 6;
+
+// if (15 < weather < 22) {
+//   console.log("pencek geyin");
+// } else if (weather > 22) {
+//   console.log("nazik geyin");
+// } else {
+//   console.log("qalin geyin");
+// }
+
+//TASK 2
+
+// let letter = "a" ;
+// if (
+//   letter === "a" ||
+//   letter === "i" ||
+//   letter === "o" ||
+//   letter === "u" ||
+//   letter === "e"
+// ) {
+//   console.log("sait");
+// } else {
+//   console.log("samit");
+// }
+
+//TASK 3
+// let number1 = 12;
+// let number2 = 15;
+// let number3 = 20;
+// if(number1>number2 && number1>number3){
+//     console.log(number1 + "is greated");
+// } else if(number2>number1 && number2>number3){
+//     console.log(number2 + "is greated");
+// }else{
+//     console.log(number3  + "is greated");
+// }
+
+// //TASK4
+// let exam = 75;
+// if(exam > 90){
+//     console.log("A");
+// }else if(exam > 80){
+//     console.log("B");
+// }else if(exam > 70){
+//     console.log("C");
+// }else if(exam > 60){
+//     console.log("D");
+// }else if(exam > 50){
+//     console.log("E");
+// }
+
+// //TASK5
+// let firtsName = "Aqafia";
+// if (firtsName.length < 5) {
+//   console.log("salam <username> . qisa adiniz var");
+// } else if (5 < firtsName.length < 10) {
+//     console.log("salam <username> . orta uzunluqlu  adiniz var");
+// } else if ( firtsName.length > 10) {
+//     console.log("salam <username> . adiniz cox uzundu");
+// }
+
+//TASK6
+// let a = 4;
+// let b = 5;
+// let c = 6;
+
+// if(a+b>a+c && a+b>b+c){
+//     console.log("big sum result a and b");
+// }else if(a+c>a+b && a+c>b+c){
+//     console.log("big sum result a and c");
+// }else if(c+b>b+a && c+b>a+c){
+//     console.log("big sum result b and c");
+// }
+
+//TASK7
+let a = 2;
+let b = 3;
+let c = 4;
+if(a*100+b*10+c>a*100+c*10+b && a*100+b*10+c>)
