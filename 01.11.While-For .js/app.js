4fbe7ffae7e107ffb6534e159050d9dcd87dc415
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-11-01 08:24:19 -0700

    loops.js

---
+++ 01.11.While-For .js/app.js
@@ -0,0 +1,55 @@
+//TASK1
+// let number = 0;
+// for(number=0;number<100;number++){
+//     if(number%2===0)
+//     console.log(number);
+// }
+//TASK2
+// let str = "qanira";
+// let i = 0;
+// for(i=0;str.length>i;i++){
+//     console.log(str[i]);
+// }
+
+//TASK3
+// let i = 0;
+// for (i = 10 ; i < 99; i++) {
+//   if (i % 10 === 7) console.log(i);
+// }
+
+//TASK4
+// let i = 0;
+// for(i=10; i<100 ;i++){
+//     if(i%11 === 0) console.log(i);
+// }
+
+//TASK5
+// let i = 25;
+
+// for (let a = 0; a <= i; a++) {
+//   if (i % a === 0) console.log(a);
+// }
+
+//TASK6
+// let i = 5;
+// let multi = 1;
+// for( let a=1;a<=i;a++){
+//    multi*=a}
+// console.log(multi);
+
+//task 7
+
+// let sum = 0;
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 === 0) sum += i;
+// }
+// console.log(sum);
+
+//TASK 8
+// let i = 10;
+// let sum = 0;
+// for(i=10;i<30;i++){
+//     if(i%3===0)
+//     sum +=i
+// }
+// console.log(sum);
