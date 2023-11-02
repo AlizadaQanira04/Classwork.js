02e7148b6328e536845bd9ae12dd148b7d9ddd1c
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-11-02 08:41:48 -0700

    Math js

---
+++ 03.Math-solution.js/app.js
@@ -0,0 +1,17 @@
+//TASK1
+console.log(Math.floor(17,23));
+console.log(Math.ceil(17,23));
+console.log(Math.round(17,23));
+console.log(Math.trunc(17,23));
+console.log(Math.sign(90));
+console.log(Math.abs(-90));
+console.log(Math.sqrt(625));
+console.log(Math.sign(-89));
+console.log(Math.pow(3,4));
+console.log(Math.pow(2,Math.trunc(17.25)));
+console.log(Math.random() *30+50);
+
+
+
+ 
+
