db98e2bb21f0aad04e75734aeccd088a8049728c
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-11-03 08:59:51 -0700

    js

---
+++ 03.11.Array&Object/app.js
@@ -0,0 +1,31 @@
+//TASK1
+// let array = [12, 45, 6, 78, 34, 99, 23];
+// let max = array[0];
+// let min = array[0];
+
+// for (let i = 1; i < array.length; i++) {
+//   if (array[i] > max) {
+//     max = array[i];
+//   } else if (array[i] < min) {
+//     min = array[i];
+//   }
+// }
+
+// console.log("MAX:", max);
+// console.log("MIN:", min);
+
+//TASK2
+// let array = [12, 45, 6, 78, 34, 99, 23];
+// let sum = 0;
+// for(let i = 0;i<array.length;i++){
+//     sum+=array[i];
+// }
+// console.log("SUM:", sum);
+
+//TASK3
+let elements = [1, 2, 3, 4, 5];
+let temp = elements[2];
+let second = elements[1];
+temp = se;
+let third = temp;
+console.log(elements);
