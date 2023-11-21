c51a97e8ab2e53b7ac1df242523b27365c2bd546
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-11-21 07:34:42 -0800

    cls

---
+++ 21.11.JS-DOM/main.js
@@ -0,0 +1,40 @@
+//TASK1
+let btn = document.querySelector(".btn ");
+
+// btn.addEventListener("click", function(){
+//   btn.parentElement.classList.add("bg", "size");
+//   btn.classList.add("btn-position");
+//   btn.parentElement.style.position = "relative";
+
+//   btn.style.position = "absolute";
+// });
+
+//TASK2
+// btn.addEventListener("click", function(){
+//   document.body.classList.toggle("bg");
+// });
+
+//TASK3
+let image = document.querySelector(".image");
+image.setAttribute("src","abstract-blurred-multi-colored-background-generative-ai_169016-30200.avif")
+
+
+
+
+
+
+//TASK4
+// const liAr = ["Ganja", "Baki", "Yevlakh", "Goranboy", "Shamkir"];
+// let ul = document.createElement("ul");
+// ul.style.listStyle="none";
+// liAr.forEach((item) => {
+//   let li = document.createElement("li");
+//   li.innerText = item;
+//   ul.append(li);
+
+//   li.addEventListener("click", function () {
+//     this.remove();
+//   });
+// });
+
+// document.querySelector(".list").append(ul);
