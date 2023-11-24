3984f94868331f589e9f2255d68446256da36c3e
AlizadeQanira04 <gd7mhxn6a@code.edu.az>
2023-11-24 06:03:11 -0800

    clswrk

---
+++ 24.11.Event.js/main.js
@@ -0,0 +1,26 @@
+let key = document.querySelectorAll(".key");
+console.log(key);
+let listOne = document.querySelector(".list one");
+let listTwo = document.querySelector(".list two");
+let listThree = document.querySelector(".list three");
+
+window.addEventListener("keydown", function (e) {
+  key.forEach((element) => {
+    console.log(element.innerText === e.key);
+    if (element.innerText === e.key) {
+      element.style.opacity = "0";
+      element.style.backgroundColor = "#06c06f";
+      element.style.transform = "scale(1.3)";
+      element.style.transition = "150ms";
+    }
+  });
+});
+window.addEventListener("keyup", function (e) {
+  key.forEach((element) => {
+    if (element.innerText === e.key) {
+      element.style.opacity = "1";
+      element.style.backgroundColor = "darkcyan  ";
+      element.style.transform = "scale(1)";
+    }
+  });
+});
